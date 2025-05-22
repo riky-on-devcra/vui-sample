4b35b44f34936bac12e31424de437a6dabf64e7b
riky <riky.kim@devcra.com>
2025-05-22 14:22:56 +0900

    deploy

--- public/rubicon.sample.js
+++ public/rubicon.sample.js
@@ -181,6 +181,7 @@
     for (let i = 0; i < localStorage.length; i++) {
       const key = localStorage.key(i);
       if (key && key.startsWith("rubicon-actions:")) {
+        console.log("[RUBICON] found actions:", key);
         const id = key.split(":")[1];
         window.rubicon.consumeActions(id);
       }
